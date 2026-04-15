30be5891b55c906984a7613c07a5315acbc1900c
Fabio Fognani <fabio.fognani@gmail.com>
2026-04-15 08:28:47 +0200

    added comments

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,3 +1,28 @@
 const baseConfig = require("@features/config-eslint").default;
 
-module.exports = [...baseConfig];
+/*
+  To extend this configuration for some specific package/app:
+
+  1. add lint scripts in package.json of the specific app/package
+      "lint": "eslint .",
+      "lint:fix": "eslint . --fix",
+
+  2. add eslint.config.cjs configuration in the specific app/package
+  // apps/some-package/eslint.config.cjs
+  const baseConfig = require("@features/config-eslint").default;
+
+  module.exports = [
+    ...baseConfig,
+    {
+      files: ["...pattern..."],
+      rules: {
+        ...customRules
+      },
+    },
+  ];
+ */
+
+module.exports = [
+  // { ignores: ["apps/some-package/**"] }, // to exclude from root config
+  ...baseConfig,
+];
